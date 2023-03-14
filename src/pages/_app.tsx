e1bd91ea2a946ed1391245397260517867d4cb38
Mami. N <mamypoco@gmail.com>
2023-03-13 20:15:54 -0700

    first commit - connect stripe

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { AppProps } from "next/app";
+import queryClient from "../lib/query";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+   return (
+      <QueryClientProvider client={queryClient}>
+         <ReactQueryDevtools initialIsOpen={false} />
+         <Component {...pageProps} />
+      </QueryClientProvider>
+   );
+}
+
+export default MyApp;
